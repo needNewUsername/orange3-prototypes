2c511a33b968b0398fcdca4af81b56fa813d9bd4
noahnovsak <noah.novsak@gmail.com>
2022-05-23 12:52:22 +0200

    moved sorting to proxy model

--- orangecontrib/prototypes/widgets/nptablemodel.py
+++ orangecontrib/prototypes/widgets/nptablemodel.py
@@ -1,3 +1,4 @@
+import copy
 from numbers import Number
 from queue import Queue, Empty
 from itertools import combinations
@@ -5,10 +6,9 @@ from types import SimpleNamespace
 from typing import Iterable
 from threading import Timer, Lock
 import time
-import tqdm
 import numpy as np
 
-from AnyQt.QtCore import QModelIndex, Qt, QAbstractTableModel
+from AnyQt.QtCore import QModelIndex, Qt, QAbstractTableModel, QSortFilterProxyModel
 from AnyQt.QtWidgets import QTableView, QVBoxLayout, QHeaderView
 
 from Orange.data import Variable, Table
@@ -23,9 +23,9 @@ class RankModel(QAbstractTableModel):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.__sortInd: np.ndarray
-        self.__sortColumn = -1
-        self.__sortOrder = Qt.DescendingOrder
+        # self.__sortInd: np.ndarray
+        # self.__sortColumn = -1
+        # self.__sortOrder = Qt.DescendingOrder
 
         self._data = None  # type: np.ndarray
         self._columns = 0
@@ -46,6 +46,101 @@ class RankModel(QAbstractTableModel):
 
     def columnCount(self, parent=QModelIndex(), *args, **kwargs):
         return 0 if parent.isValid() else self._columns
+
+    def data(self, index, role=Qt.DisplayRole):
+        if not index.isValid():
+            return
+
+        # row = self.mapToSourceRows(index.row())
+        row = index.row()
+        col = index.column()
+
+        try:
+            value = self._data[row, col]
+            if role == Qt.EditRole:
+                return value
+            if col >= self._columns - 2:
+                value = self._domain[value]
+        except IndexError:
+            return
+        if role == Qt.DecorationRole and isinstance(value, Variable):
+            return gui.attributeIconDict[value]
+        if role == Qt.DisplayRole:
+            if isinstance(value, Number):
+                return "{:.1f}%".format(100 * value)
+            return str(value)
+        if role == Qt.TextAlignmentRole and isinstance(value, Number):
+            return Qt.AlignRight | Qt.AlignVCenter
+        if role == Qt.ToolTipRole:
+            return str(value)
+
+    def setHorizontalHeaderLabels(self, labels):
+        self._headers[Qt.Horizontal] = tuple(labels)
+
+    def headerData(self, section, orientation, role=Qt.DisplayRole):
+        headers = self._headers.get(orientation)
+        if headers and section < len(headers) and role == Qt.DisplayRole:
+            return headers[section]
+        return super().headerData(section, orientation, role)
+
+    def initialize(self, data):
+        self.beginResetModel()
+        self._data = np.array(data)
+        self._rows, self._columns = self._data.shape
+        # self.unsort()
+        self.endResetModel()
+
+    def clear(self):
+        self.beginResetModel()
+        self._data = None
+        self._rows = self._columns = 0
+        # self.unsort()
+        self.endResetModel()
+
+    def append(self, rows):
+        if not isinstance(self._data, np.ndarray):
+            return self.initialize(rows)
+
+        n_rows = len(rows)
+        n_data = len(self._data)
+        insert = self._rows < self._max_rows
+
+        if insert:
+            self.beginInsertRows(QModelIndex(), self._rows, min(self._max_rows, self._rows + n_rows) - 1)
+
+        if self._rows + n_rows >= n_data:
+            n_data = min(max(n_data + n_rows, 2 * n_data), self._n_comb)
+            ar = np.full((n_data, self._columns), np.nan)
+            ar[:self._rows] = self._data[:self._rows]
+            self._data = ar
+
+        self._data[self._rows:self._rows + n_rows] = rows
+        self._rows += n_rows
+
+        if insert:
+            self.endInsertRows()
+
+        # if self.__sortColumn >= 0:
+        #     self.sort_new(n_rows)
+
+    def __len__(self):
+        return self._rows
+
+    def source_data(self):
+        return self._data
+
+
+class ModelProxy(QSortFilterProxyModel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.__sortInd: np.ndarray
+        self.__sortColumn = -1
+        self.__sortOrder = Qt.DescendingOrder
+
+    def setSourceModel(self, model):
+        super().setSourceModel(model)
+        self._data = model.source_data()
 
     def sort(self, column: int, order=Qt.DescendingOrder):
         if self._data is None:
@@ -94,83 +189,8 @@ class RankModel(QAbstractTableModel):
             return self.__sortInd[rows]
         return rows
 
-    def data(self, index, role=Qt.DisplayRole):
-        if not index.isValid():
-            return
-
-        row = self.mapToSourceRows(index.row())
-        col = index.column()
-
-        try:
-            value = self._data[row, col]
-            if role == Qt.EditRole:
-                return value
-            if col >= self._columns - 2:
-                value = self._domain[value]
-        except IndexError:
-            return
-        if role == Qt.DecorationRole and isinstance(value, Variable):
-            return gui.attributeIconDict[value]
-        if role == Qt.DisplayRole:
-            if isinstance(value, Number):
-                return "{:.1f}%".format(100 * value)
-            return str(value)
-        if role == Qt.TextAlignmentRole and isinstance(value, Number):
-            return Qt.AlignRight | Qt.AlignVCenter
-        if role == Qt.ToolTipRole:
-            return str(value)
-
-    def setHorizontalHeaderLabels(self, labels):
-        self._headers[Qt.Horizontal] = tuple(labels)
-
-    def headerData(self, section, orientation, role=Qt.DisplayRole):
-        headers = self._headers.get(orientation)
-        if headers and section < len(headers) and role == Qt.DisplayRole:
-            return headers[section]
-        return super().headerData(section, orientation, role)
-
-    def initialize(self, data):
-        self.beginResetModel()
-        self._data = np.array(data)
-        self._rows, self._columns = self._data.shape
-        self.unsort()
-        self.endResetModel()
-
-    def clear(self):
-        self.beginResetModel()
-        self._data = None
-        self._rows = self._columns = 0
-        self.unsort()
-        self.endResetModel()
-
-    def append(self, rows):
-        if not isinstance(self._data, np.ndarray):
-            return self.initialize(rows)
-
-        n_rows = len(rows)
-        n_data = len(self._data)
-        insert = self._rows < self._max_rows
-
-        if insert:
-            self.beginInsertRows(QModelIndex(), self._rows, min(self._max_rows, self._rows + n_rows) - 1)
-
-        if self._rows + n_rows >= n_data:
-            n_data = min(max(n_data + n_rows, 2 * n_data), self._n_comb)
-            ar = np.full((n_data, self._columns), np.nan)
-            ar[:self._rows] = self._data[:self._rows]
-            self._data = ar
-
-        self._data[self._rows:self._rows + n_rows] = rows
-        self._rows += n_rows
-
-        if insert:
-            self.endInsertRows()
-
-        if self.__sortColumn >= 0:
-            self.sort_new(n_rows)
-
-    def __len__(self):
-        return self._rows
+    def mapToSource(self, index):
+        return index
 
 
 class Widget(OWWidget, ConcurrentWidgetMixin):
@@ -184,9 +204,10 @@ class Widget(OWWidget, ConcurrentWidgetMixin):
         OWWidget.__init__(self)
         ConcurrentWidgetMixin.__init__(self)
 
-        self.N = 0
+        self.attrs = 0
         self.progress = 0
-        self.add_to_model = Queue()
+        self.running = False
+        self.saved_state = None
 
         self.setLayout(QVBoxLayout())
 
@@ -194,43 +215,50 @@ class Widget(OWWidget, ConcurrentWidgetMixin):
         self.model.setHorizontalHeaderLabels([
             "Score 1", "Feature 1", "Feature 2"
         ])
+        self.proxy = ModelProxy()
+        self.proxy.setSourceModel(self.model)
         view = QTableView(selectionBehavior=QTableView.SelectRows,
-            selectionMode=QTableView.SingleSelection,
-            showGrid=False,
-            editTriggers=gui.TableView.NoEditTriggers)
+                          selectionMode=QTableView.SingleSelection,
+                          showGrid=False,
+                          editTriggers=gui.TableView.NoEditTriggers)
         view.setSortingEnabled(True)
         view.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
-        view.setModel(self.model)
+        view.setModel(self.proxy)
 
-        self.button = gui.button(self, self, "do stuff", callback=self.push_button)
+        self.button = gui.button(self, self, "do stuff", callback=self.toggle)
 
         self.layout().addWidget(view)
         self.layout().addWidget(self.button)
 
-    def push_button(self):
-        self.button.setDisabled(True)
-        self.start(do_stuff, compute_score, self.N)
+    def toggle(self):
+        self.running = not self.running
+        if self.running:
+            self.button.setText("Pause")
+            self.button.repaint()
+            self.progressBarInit()
+            self.start(run, compute_score,
+                       self.attrs, self.iterate_states,
+                       self.saved_state, self.progress)
+        else:
+            self.button.setText("Continue")
+            self.button.repaint()
+            self.cancel()
+            self.progressBarFinished()
 
     def on_partial_result(self, result):
-        try:
-            while True:
-                queued = result.queue.get_nowait()
-                self.add_to_model.put_nowait(queued)
-        except Empty:
-            pass
-
         rows = []
         try:
             while True:
-                queued = self.add_to_model.get_nowait()
+                queued = result.get_nowait()
+                self.saved_state = queued.next_state
                 row_item = list(queued.score) + list(queued.state)
                 rows.append(row_item)
         except Empty:
             if rows:
                 self.model.append(rows)
 
-        self.progress = self.model._rows
-        self.progressBarSet(int(self.progress * 100 / (self.N*(self.N-1)//2)))
+        self.progress = len(self.model)
+        self.progressBarSet(int(self.progress * 100 / (self.attrs*(self.attrs-1)//2)))
 
     def on_done(self, result):
         self.button.setText("done")
@@ -238,62 +266,67 @@ class Widget(OWWidget, ConcurrentWidgetMixin):
     @Inputs.data
     def set_data(self, data):
         self.model.set_domain(data)
-        self.N = len(data.domain.attributes)
+        self.attrs = len(data.domain.attributes)
 
-
-class ItemStore(object):
-    def __init__(self):
-        self.lock = Lock()
-        self.items = []
-
-    def put(self, item):
-        with self.lock:
-            self.items.append(item)
-
-    def put_nowait(self, item):
-        return self.put(item)
-
-    def get(self):
-        with self.lock:
-            items, self.items = self.items, []
-        return items
-
-
-class Result(SimpleNamespace):
-    queue = None  # type: Queue[QueuedScore, ...]
-    # scores = None  # type: Optional[List[float, ...]]
+    def iterate_states(self, initial_state):
+        si, sj = initial_state or (0, 0)
+        for i in range(si, self.attrs):
+            for j in range(sj, i):
+                yield i, j
+            sj = 0
 
 
 class QueuedScore(SimpleNamespace):
-    # position = None  # type: int
     score = None  # type: float
     state = None  # type: Iterable
-    # next_state = None  # type: Iterable
+    next_state = None  # type: Iterable
 
 
 def compute_score(state):
-    time.sleep(0.0000001)
+    time.sleep(1e-7)
     return sum(state),
 
 
-def do_stuff(compute, N, task):
+def run(compute, attrs, iterate_states, saved_state, progress, task):
+    task.set_status("Getting combinations...")
+    task.set_progress_value(0.1)
+    states = iterate_states(saved_state)
+
     task.set_status("Getting scores...")
-    res = Result(queue=Queue())
+    queue = Queue()
     can_set_partial_result = True
+
+    def do_work(st, next_st):
+        try:
+            score = compute(st)
+            if score is not None:
+                queue.put_nowait(QueuedScore(score=score, state=st, next_state=next_st))
+        except Exception:
+            pass
 
     def reset_flag():
         nonlocal can_set_partial_result
         can_set_partial_result = True
 
-    for state in tqdm.tqdm(combinations(range(N), 2), total=N*(N-1)//2):
-        score = compute(state)
-        res.queue.put_nowait(QueuedScore(score=score,
-                                         state=state))
-        if can_set_partial_result:
-            task.set_partial_result(res)
-            can_set_partial_result = False
-            Timer(0.05, reset_flag).start()
-    task.set_partial_result(res)
+    state = None
+    next_state = next(states)
+    try:
+        while True:
+            if task.is_interruption_requested():
+                return queue
+            task.set_progress_value(progress * 100 // (attrs*(attrs-1)//2))
+            progress += 1
+            state = copy.copy(next_state)
+            next_state = copy.copy(next(states))
+            do_work(state, next_state)
+            if can_set_partial_result:
+                task.set_partial_result(queue)
+                can_set_partial_result = False
+                Timer(0.05, reset_flag).start()
+    except StopIteration:
+        do_work(state, None)
+        task.set_partial_result(queue)
+    return queue
 
 
 if __name__ == "__main__":
